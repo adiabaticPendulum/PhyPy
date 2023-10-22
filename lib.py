aae19bf8b123825e4e2cd5df6d21a6f27d0c634a
Lasse Peters <heisenberg.industries.web@gmail.com>
2023-10-22 14:43:06 +0200

    Refactored Formulat to make it inherit MatEx, conversion of Formula to Val, outputting Dataset to CSV and Latex

--- lib.py
+++ lib.py
@@ -17,8 +17,10 @@ import pyppeteer as pt
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import pandas.io.formats.style as sty
 import latex2sympy2 as l2s2
 import sympy as smp
+import re
 
 libs = [asyncio, mt, pt, plt, np, pd, dc, cpy, l2s2]
 
@@ -51,9 +53,10 @@ def _warn(name, description):
 
 def _error(name, description):
     print(
-        "\n" + ESC_STYLES["Error"] + "Error: " + name + ESC_STYLES["Default"] + " " + ESC_STYLES[
+        "\n\n\n(" + ESC_STYLES["Error"] + "Error: " + name + ESC_STYLES["Default"] + " " + ESC_STYLES[
             "Error_txt"] + description +
-        ESC_STYLES["Default"] + "\n")
+        ESC_STYLES["Default"] + ")\n\n\n")
+    raise Exception(name + ": " + description)
 
 
 ########################################################################################################
@@ -116,11 +119,7 @@ class Val:  # Todo: document!
         self._e_known_decimal_figures = 0 if "." not in str(err) else len(str(err).split('.')[1])
 
     def __str__(self):
-        try:
-            rnd = " (\\approx " + self.sig_round(ignore_errors=True)[0] + ")"
-        except:
-            rnd = " (No rounding available)"
-        return str(self.v) + " \\pm " + str(self.e) + rnd
+        return str(self.v) if mt.isnan(self.e) or type(self.e) == str else self.sig_round()
 
     def __float__(self):
         return float(self.v)
@@ -198,15 +197,16 @@ class Var:
 
 
 class MatEx:
-    vars = {}
+    variables = {}
 
     @property
     def sympy(self):
-        return self._sympy.doit()
+        return self._sympy
 
     @sympy.setter
     def sympy(self, new_sympy):
-        self._sympy = new_sympy
+        self._raw_sympy = new_sympy
+        self._sympy = self._raw_sympy.doit()
         self._latex = l2s2.latex(self.sympy)
 
     @property
@@ -217,52 +217,67 @@ class MatEx:
     def latex(self, new_latex):
         self.sympy = l2s2.latex2sympy(new_latex)
 
-    @property
-    def raw_sympy(self):
-        return self._sympy
-
     def __init__(self, variables, latex="", sympy=None):
-        self.sympy = sympy if sympy is not None else (l2s2.latex2sympy(latex) if not latex == "" else None)
+        self.sympy = sympy if sympy is not None else (None if latex == "" else l2s2.latex2sympy(latex))
         for var in variables:
             if type(var) is str:
-                self.vars[var] = Var(var)
+                self.variables[var] = Var(var)
             else:
-                self.vars[var.n] = var
+                self.variables[var.n] = var
 
     def __str__(self):
         return self.latex
 
     def at(self, var_val_pairs):
+        tmp_sympy = self.sympy.doit()
         for var_val_pair in var_val_pairs:
-            self.sympy = self.sympy.subs(var_val_pair[0], var_val_pair[1])
-        return self.sympy
+            tmp_sympy = tmp_sympy.subs(var_val_pair[0], var_val_pair[1])
+        return tmp_sympy
 
 
-class Formula:
-    @property
-    def expr(self):
-        return self._expr
+class Formula(MatEx):
 
-    @expr.setter
-    def expr(self, new_expr):
-        self._expr = new_expr
+    @staticmethod
+    def from_mat_ex(mat_ex):
+        return Formula(variables=list(mat_ex.variables.keys()), sympy=mat_ex.sympy)
+
+    def _update_errors(self):
         _err = 0
-        for key in self._expr.vars.keys():
-            _err += (smp.Derivative(self._expr.sympy, self._expr.vars[key].n) * self._expr.vars[key].n) ** 2
-        self.error = MatEx(variables=list(self._expr.vars.values()), sympy=smp.sqrt(_err))
+        for key in self.variables.keys():
+            _err += (smp.Derivative(self.sympy, self.variables[key].n) * self.variables[key].n) ** 2
+        self.error = MatEx(variables=list(self.variables.keys()), sympy=smp.sqrt(_err))
 
-    def __init__(self, variables=[], latex="", sympy=None, mat_ex=None):
-        self.expr = MatEx(variables, latex, sympy) if MatEx is None else mat_ex
+    @MatEx.sympy.setter
+    def sympy(self, new_sympy):
+        MatEx.sympy.fset(self, new_sympy)
+        self._update_errors()
+
+    @MatEx.latex.setter
+    def latex(self, new_latex):
+        MatEx.latex.fset(self, new_latex)
+        self._update_errors()
+
+    def __init__(self, variables=[], latex="", sympy=None):
+        MatEx.__init__(self, variables, latex, sympy)
 
     def __str__(self):
-        return str(self.expr) + " \pm " + str(self.error)
+        return MatEx.__str__(self) + " \pm " + str(self.error)
 
     def at(self, var_val_pairs):
-        return self.expr.at(var_val_pairs), self.error.at(var_val_pairs)
+        return MatEx.at(self, var_val_pairs), self.error.at(var_val_pairs)
 
-    #def create_values(self, ranges):
-        #TODO
+    def to_val(self, var_val_pairs):
+        at = self.at(var_val_pairs)
+        try:
+            return Val(str(at[0].evalf()), str(at[1].evalf()))
+        except dc.InvalidOperation:
+            _error(name="ConversionError",
+                   description="Can't convert sympy to Val. Make shure that the specified key-value-pairs are providing values for all used variables, so that the sympy-expressioon can be evaluated to a numeric expression and doesn't contain any unset variables.")
 
+    def clone(self):
+        return cpy.copy(self)
+
+    # def create_values(self, ranges):
 
 
 class Dataset:  # Object representing a full Dataset
@@ -334,7 +349,8 @@ class Dataset:  # Object representing a full Dataset
             for c_index in c_indices:
                 self.set(r_index, c_index, method(self.at(r_index, c_index), r_index, c_index))
 
-    # def disp(self):
+    def print(self):
+        print(self.frame)
 
     def delete(self, c_indices=None, r_indices=None):
         if c_indices is None:
@@ -483,17 +499,35 @@ class Dataset:  # Object representing a full Dataset
         self.frame = temp.loc[userows]
         self.apply(lambda obj, r_index, c_index: to_val(obj))
 
+    def to_csv(self, path, delimiter=";", columns=None):
+        if columns is None:
+            columns = self.get_col_names()
+        self.frame.to_csv(path, sep=delimiter, index_label=self.x_label, columns=columns)
 
-##################################################
+    def to_latex(self, show_index=True):
+        styler = sty.Styler(self.frame)
+        if not show_index:
+            styler.hide(axis="index")
+
+        col_frmt = "|l|"
+        for col in self.get_col_names():
+            col_frmt += "l|"
+
+        ltx = styler.to_latex(position_float="centering", label="tab:my_table", caption="\\todo{caption}",
+                              column_format=col_frmt)
+        ltx = ltx.replace("\\begin{tabular}{" + col_frmt + "}", "\\begin{tabular}{" + col_frmt + "}" + "\n\hline")
+        ltx = ltx.replace("\\\\", "\\\\ \hline", 1)
+        ltx = ltx.replace("\end{tabular}", "\hline\n\end{tabular}")
+        return ltx
+
+    ##################################################
+
+
 # Testing
-expr = MatEx(["x", "y"], "x^2 + y")
-print(expr)
-# print(expr.latex)
-# print(expr.at([["x", 2], ["y", 5]]))
-# expr.latex = "2x+5y"
-print(expr.sympy, " oder ", expr)
-f = Formula(mat_ex=expr)
-print(f.at([["x", 3], ["y", 4]]))
+ds = Dataset(r_names=["x", "y", "z"], lists=[[1, 2, 3], [3, 4, 5]])
+ds.print()
+ds.to_csv("../test.csv")
+print(ds.to_latex(show_index=True))
 ###################################################################################################
 # Best motivateMe() texts:
 # I understand that your physics laboratory courses may be draining and downright boring, but don't let these setbacks deter you from your goals. Your previous progress on the lab report was phenomenal, and that is a true testament to your intelligence and hardworking nature. Though the courses may not be implemented as efficiently as they should be, do not let this dull your passions. Remain focused and committed to your goals, and you will successfully complete the lab report in no time. Keep striving for greatness!
